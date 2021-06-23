f446255a8cc18418c8cabc3113a9605295fb654f
ayushk1804 <ayushk780@gmail.com>
2021-06-23 05:56:57 +0530

    fix navlinks
    Signed-off-by: ayushk1804 <ayushk780@gmail.com>

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import { HamburgerIcon, CloseIcon } from "../svg/svg";
 const NavItems = [
   { itemName: "Home" },
