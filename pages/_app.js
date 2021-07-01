9a531eff7a72e39024093e286e19d3a52a34878b
ayushk1804 <ayushk780@gmail.com>
2021-07-01 12:33:36 +0530

    Added google Analytics and improved SEO
    Signed-off-by: ayushk1804 <ayushk780@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-// import 'tailwindcss/tailwind.css'
-import '../style/global.css'
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../style/global.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../src/lib/gtag";
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
+
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ export default function Home() {
   useEffect(() => {
     const navLogo = document.getElementById("nav-logo");
     const siteContent = document.getElementById("site-content");
-    const headerRegister = document.getElementById("header-register-button");
     const navRegister = document.getElementById("nav-register-button");
     siteContent.addEventListener("scroll", () => {
       if (siteContent.scrollTop > 500) {
@@ -62,12 +61,28 @@ export default function Home() {
       </video>
       <Head>
         <title>Advaith HackFest</title>
-        <meta property="og:title" content="Advaith Hackfest" key="title" />
+        <meta
+          property="og:title"
+          content="Register || Advaith Hackfest"
+          key="title"
+        />
         <meta
           property="og:description"
           name="description"
-          content="Advaith Hackfest - PointBlank"
+          content="Register || 20-26 July 2021 HackFest || Advaith Hackfest - PointBlank"
         />
+        <meta
+          property="og:image"
+          content="/Assets/Images/advaith-yeah-banner.png"
+        />
+        <meta property="og:url" content="https://fest.dsce.in/" />
+        <meta property="og:site_name" content="PointBlank.club" />
+        <meta
+          name="twitter:image:alt"
+          content="Register now https://fest.dsce.in || 20-26 July 2021 HackFest"
+        />
+        <meta name="twitter:card" content="20-26 July 2021 HackFest" />
+        <meta name="twitter:site" content="@pointblank_dsce" />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
