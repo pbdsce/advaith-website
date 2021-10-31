abd1f781aed110131275f7ffedea974c291dc598
ayushk1804 <ayushk780@gmail.com>
2021-10-31 09:48:20 +0530

    Added Github Logo
    Signed-off-by: ayushk1804 <ayushk780@gmail.com>

--- src/Components/IndexPageComponents/CTAButton/CTAButton.js
+++ src/Components/IndexPageComponents/CTAButton/CTAButton.js
@@ -1,16 +1,21 @@
+import Link from 'next/link'
 const CTAButton = () => {
   return (
     <div className="sticky float-right inset-0 flex place-items-center p-5 md:hover:animate-none transition duration-200 ease-in-out">
-      <button disabled={true} className="rounded-full p-2 md:p-3 md:m-5 bg-gray-500 focus:outline-none cursor-not-allowed">
-        <div className="flex place-items-center rounded-full h-8 w-8">
-          <img
-            src="/Assets/Images/Brands/Discord-Logo-White.svg"
-            width={30}
-            height={30}
-            className="object-contain m-auto"
-            alt="Discord-logo"
-          />
-        </div>
+      <button className="rounded-full p-1 md:p-3 md:m-5 bg-white focus:outline-none">
+        <Link href="https://github.com/pbdsce/advaith-website">
+          <a>
+            <div className="flex place-items-center rounded-full h-8 w-8">
+              <img
+                src="/Assets/Images/Brands/github.svg"
+                width={30}
+                height={30}
+                className="object-contain m-auto"
+                alt="Github"
+              />
+            </div>
+          </a>
+        </Link>
       </button>
     </div>
   );
